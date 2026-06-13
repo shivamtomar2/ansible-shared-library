45407512d7f02c2ba79ded097c7cc54361c27380
Shivam Tomar <shivamtomar2000@gmail.com>
2026-06-13 11:04:04 +0530

    Refactor deployment config to return a map

--- deployment-config.groovy
+++ deployment-config.groovy
@@ -1,6 +1,14 @@
-SLACK_CHANNEL_NAME = "jenkins-alerts"
-ENVIRONMENT = "prod"
-CODE_BASE_PATH = "playbooks"
-PLAYBOOK_NAME = "deploy.yml"
-ACTION_MESSAGE = "Kafka Deployment Completed Successfully"
-KEEP_APPROVAL_STAGE = true
+return [
+
+    SLACK_CHANNEL_NAME : "jenkins-alerts",
+
+    ENVIRONMENT : "prod",
+
+    CODE_BASE_PATH : "playbooks",
+
+    PLAYBOOK_NAME : "deploy.yml",
+
+    ACTION_MESSAGE : "Kafka Deployment Completed Successfully",
+
+    KEEP_APPROVAL_STAGE : true
+]
